d49db3c1fa416faff7ceacc710fdcbb20069d469
adipiscingelite <syaifulrahmatdani.18>
2024-02-21 13:15:28 +0700

    first

---
+++ tiket.js
@@ -0,0 +1,21 @@
+const nama = prompt('Masukkan nama: ')
+const umur = prompt('Masukkan umur: ')
+
+console.log(nama)
+console.log(umur)
+
+if (umur >= 13){
+  let studio = prompt('Pilih studio: (A, B, atau C)')
+  
+  if(studio == 'A'){
+    alert(`Kamu memilih studio A`)
+  }else if(studio == 'B'){
+    alert(`Kamu memilih studio B`)
+  }else if(studio == 'C'){
+    alert(`Kamu memilih studio C`)
+  }else{
+    alert('Pilihan tidak valid')
+  }
+}else{
+  alert('Kamu tidak boleh masuk studio')
+}
